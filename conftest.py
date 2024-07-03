7659d9a0c28fde8ed3f49c5f84a47f6e821c9016
Aleksei Ostrovskii <a.a.ostrovskiy@mail.ru>
2024-07-03 22:52:54 +0300

    Second commit for Jenkins with selenoid

--- conftest.py
+++ conftest.py
@@ -2,19 +2,45 @@ import pytest
 from selene import browser
 from selenium import webdriver
 import allure
-
+import os
+from selenium.webdriver.chrome.options import Options
 from utils import attach
 
+# Без селиноида, локально:
+# @pytest.fixture(scope="function")
+# def open_browser():
+#     # browser.config.driver_name = 'firefox'
+#     # browser.config.driver_options = webdriver.FirefoxOptions()
+#     browser.open('https://ostrovok.ru/')
+#     yield browser
+#     attach.add_html(browser)
+#     attach.add_logs(browser)
+#     attach.add_screenshot(browser)
+#     browser.quit()
 
 @pytest.fixture(scope="function")
-def open_browser():
-    # browser.config.driver_name = 'firefox'
-    # browser.config.driver_options = webdriver.FirefoxOptions()
+def open_selenoid(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100",
+        "selenoid:options": {
+            "enableVideo": True,
+            "enableVNC": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
     browser.open('https://ostrovok.ru/')
     yield browser
     attach.add_html(browser)
     attach.add_logs(browser)
     attach.add_screenshot(browser)
     # Только для селеноида:
-    # attach.add_video(browser)
+    attach.add_video(browser)
     browser.quit()
